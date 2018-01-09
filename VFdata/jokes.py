3a72596c6c2d6adb50c397522799baaf8a455660
Articuno1234 <Articuno1234@users.noreply.github.com>
2018-01-09 22:58:16 +1100

    VF Update!!!!

---
+++ VFdata/jokes.py
@@ -0,0 +1,3 @@
+import random
+
+jokes = ["How do all the oceans say hello to each other?\n\nThey Wave", "What do you call a bear with no teeth?\n\nA Gummy bear", "What did one wall say to the other wall?\n\nMeet you at the corner", "What do you call cheese that isn't yours?\n\nNacho Cheese", "Where do cows go for entertainment?\n\nThe Moo-vies", "What do you call a pig that knows Karate?\n\nA Pork Chop!", "Why did the fish jump out of the water?\n\nBecause the sea-weed", "Why are Ghosts bad liars?\n\nBecaue you can see right through them"]
---
+++ VFdata/speach.py
@@ -0,0 +1,1 @@
+text = ["What is your favourite food", "Hello!", "How are you?", "Good", "Ok", "No", "Yes"]
